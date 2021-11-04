f993f6077219a90628788dae527a1c51717bd2aa
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-04 09:19:07 +0100

    feat: make add_reminder accept a conforming reminder class

--- src/database.py
+++ src/database.py
@@ -1,6 +1,5 @@
 import csv
-from src.reminder import PoliteReminder
-from src.deadlined_reminders import DateReminder
+from src.deadlined_reminders import DeadlinedReminder
 
 
 def list_reminders():
@@ -16,8 +15,11 @@ def list_reminders():
         print()
 
 
-def add_reminder(text, date):
-    reminder = DateReminder(text, date)
+def add_reminder(text, date, ReminderClass):
+    if not issubclass(ReminderClass, DeadlinedReminder):
+        raise TypeError("Invalid Reminder Class")
+
+    reminder = ReminderClass(text, date)
 
     with open("reminders.csv", "a+", newline="\n") as file:
         writer = csv.writer(file)

aab41fdc877ccc4328b67759812b15d7d6f0e631
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-04 09:10:23 +0100

    feat: update the database and the interface

--- app.py
+++ app.py
@@ -6,38 +6,42 @@ def handle_input():
     if choice == "3":
         return False
 
-    if(choice == "1"):
+    if choice == "1":
         list_reminders()
 
-    elif(choice == "2"):
+    elif choice == "2":
         print()
         reminder = input("What would you like to be reminded about?: ")
+        date = input("When is that due?: ")
 
-        add_reminder(reminder)
+        add_reminder(reminder, date)
         list_reminders()
     else:
         print("Invalid menu option")
 
     return True
 
+
 def print_menu():
     print()
-    print('|--------------|')
-    print('|  Pluralsight |')
-    print('|   Reminders  |')
-    print('|     App      |')
-    print('|--------------|')
-    print('* * * * * * * * *')
-    print('Please select an option:')
+    print("|--------------|")
+    print("|  Pluralsight |")
+    print("|   Reminders  |")
+    print("|     App      |")
+    print("|--------------|")
+    print("* * * * * * * * *")
+    print("Please select an option:")
     print()
-    print('1) List reminders')
-    print('2) Add a reminder')
-    print('3) Exit')
+    print("1) List reminders")
+    print("2) Add a reminder")
+    print("3) Exit")
+
 
 def main():
     print_menu()
     while handle_input():
         print_menu()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import csv
 from src.reminder import PoliteReminder
+from src.deadlined_reminders import DateReminder
 
 
 def list_reminders():
@@ -15,9 +16,9 @@ def list_reminders():
         print()
 
 
-def add_reminder(text):
-    reminder = PoliteReminder(text)
+def add_reminder(text, date):
+    reminder = DateReminder(text, date)
 
     with open("reminders.csv", "a+", newline="\n") as file:
         writer = csv.writer(file)
-        writer.writerow([reminder.text])
+        writer.writerow(reminder)

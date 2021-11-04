4a6a26365efbe12363ea1a214360da81f01ffcb8
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-04 08:55:08 +0100

    feat: implement an abstract base class by extending the ABC class

--- src/deadline_reminders.py
+++ src/deadline_reminders.py
@@ -1,8 +1,14 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from collections.abc import Iterable
 
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+    @abstractmethod
+    def is_due(self):
+        pass
+
+
+class DeadlinedReminder(ABC, Iterable):
     @abstractmethod
     def is_due(self):
         pass
